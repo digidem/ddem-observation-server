e66395b6ab33ed210216a36ecd1f2afbd62d5bc0
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2016-12-07 21:04:19 -1000

    read images back

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var mkdirp = require('mkdirp')
 var level = require('level')
 var osmdb = require('osm-p2p')
 var osmobs = require('osm-p2p-observations')
+var mime = require('mime')
 
 var osmdir = path.join(ospath.data(), 'mapfilter-osm-p2p')
 var mediadir = path.join(osmdir, 'media')
@@ -23,7 +24,14 @@ var obs = osmobs({ db: obsdb, log: osm.log })
 var archive = require('./lib/drive.js')(drivedb, { dir: mediadir })
 
 var router = require('routes')()
-router.addRoute('GET /media/:id', function (req, res, m) {
+router.addRoute('GET /media/:file', function (req, res, m) {
+  var r = archive.createFileReadStream(m.params.file)
+  r.once('error', function (err) {
+    res.setHeader('content-type', 'text/plain')
+    res.end(err + '\n')
+  })
+  res.setHeader('content-type', mime.lookup(m.params.file))
+  r.pipe(res)
 })
 router.addRoute('POST /upload/jpg', function (req, res, m) {
   var r = through()
@@ -46,15 +54,14 @@ router.addRoute('POST /upload/jpg', function (req, res, m) {
   function fromDate (date) {
     var hex = randombytes(4).toString('hex')
     var file = strftime('%F-%H.%M.%S', date) + '-' + hex + '.jpg'
-    console.log('create file', file)
     var w = archive.createFileWriteStream(file, { live: false })
     w.on('error', function (err) {
-      console.log('ERR', err)
+      console.error('ERROR', err)
       res.statusCode = 500
       res.end(err + '\n')
     })
     w.once('finish', function () { // doesn't work
-      res.end(file)
+      res.end(file + '\n')
     })
     r.pipe(w)
   }
