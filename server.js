58a8bb749087e8c91cb7015ea7686d6951587019
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2016-12-07 21:47:21 -1000

    stream issues with uploads

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var level = require('level')
 var osmdb = require('osm-p2p')
 var osmobs = require('osm-p2p-observations')
 var mime = require('mime')
+var pump = require('pump')
 
 var osmdir = path.join(ospath.data(), 'mapfilter-osm-p2p')
 var mediadir = path.join(osmdir, 'media')
@@ -24,28 +25,44 @@ var obs = osmobs({ db: obsdb, log: osm.log })
 var archive = require('./lib/drive.js')(drivedb, { dir: mediadir })
 
 var router = require('routes')()
+router.addRoute('GET /media', function (req, res, m) {
+  res.setHeader('content-type', 'text/plain')
+  pump(archive.list({ live: false }),
+    through.obj(write), res, done)
+  function done (err) {
+    if (err) {
+      res.statusCode = 500
+      res.end(err + '\n')
+    }
+  }
+  function write (row, enc, next) {
+    next(null, row.name + '\n')
+  }
+})
 router.addRoute('GET /media/:file', function (req, res, m) {
   var r = archive.createFileReadStream(m.params.file)
   r.once('error', function (err) {
     res.setHeader('content-type', 'text/plain')
+    res.statusCode = 404
     res.end(err + '\n')
   })
   res.setHeader('content-type', mime.lookup(m.params.file))
   r.pipe(res)
 })
 router.addRoute('POST /upload/jpg', function (req, res, m) {
-  var r = through()
-  var j = jpeg()
-  j.pipe(through.obj(write, end))
-  req.pipe(j)
-  req.pipe(r)
+  var r = pump(req, through())
+  pump(req, jpeg(), through.obj(write, end), function (err) {
+    if (err && err.message !== 'premature close') {
+      res.statusCode = 500
+      res.end(err + '\n')
+    }
+  })
 
   function write (marker, enc, next) {
     if (marker.type === 'EXIF') {
-      fromDate(marker.image.ModifyDate || new Date)
-      this.destroy()
-    }
-    next()
+      var d = marker.exif.DateTimeOriginal || marker.image.ModifyDate
+      fromDate(d || new Date)
+    } else next()
   }
   function end () {
     fromDate(new Date)
@@ -54,13 +71,14 @@ router.addRoute('POST /upload/jpg', function (req, res, m) {
   function fromDate (date) {
     var hex = randombytes(4).toString('hex')
     var file = strftime('%F-%H.%M.%S', date) + '-' + hex + '.jpg'
+    console.log(file)
     var w = archive.createFileWriteStream(file, { live: false })
     w.on('error', function (err) {
-      console.error('ERROR', err)
       res.statusCode = 500
       res.end(err + '\n')
     })
     w.once('finish', function () { // doesn't work
+      console.log('FINISH')
       res.end(file + '\n')
     })
     r.pipe(w)
