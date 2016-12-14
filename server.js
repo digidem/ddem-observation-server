a55a09de30edffb1f6fda6ae05255f6bd144a883
Seth Fitzsimmons <seth@mojodna.net>
2016-12-14 12:40:45 -0800

    CORS support

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require('http')
 var ospath = require('ospath')
 var path = require('path')
+var Corsify = require('corsify')
 
 var osmdir = path.join(ospath.data(), 'mapfilter-osm-p2p')
 var obs = require('./')(osmdir)
-var server = http.createServer(obs)
+var server = http.createServer(Corsify(obs))
 server.listen(3210)
